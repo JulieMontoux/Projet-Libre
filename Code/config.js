7824c4ee5e1e6a13340cffd862ee94a79c771922
Julie montoux <julie.montoux@icloud.com>
2023-06-16 10:09:06 +0200

    Essaie de modification config pour une connexion avec nodejs

--- Code/config.js
+++ Code/config.js
@@ -1,28 +1,17 @@
-const mysql = require('mysql');
+var mysql = require('mysql'); 
 
-const config = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'vente'
-};
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "vente"
+});
 
-// Création de la connexion à la base de données
-const connection = mysql.createConnection(config);
-
-// Fonction pour exécuter une requête SQL
-function executeQuery(query, callback) {
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Erreur lors de l\'exécution de la requête :', error);
-      callback(error, null);
-      return;
-    }
-    callback(null, results);
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
   });
-}
-
-module.exports = {
-  connection,
-  executeQuery
-};
+});
