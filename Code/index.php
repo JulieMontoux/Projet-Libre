b42f94cedee9ca82cd4e0faaf012be89488eab64
Julie montoux <julie.montoux@icloud.com>
2023-06-19 15:04:48 +0200

    Ajout pour avoir l'historique des ventes ainsi que la revu du style

--- Code/index.php
+++ Code/index.php
@@ -2,238 +2,206 @@
 <html>
 <head>
   <title>Vente de Fruits</title>
+  <meta charset="utf-8">
   <style>
+    /* CSS pour la mise en page */
     body {
       font-family: Arial, sans-serif;
     }
 
-    header {
-      background-color: #333;
-      color: #fff;
-      padding: 20px;
-      text-align: center;
-    }
-
     h1 {
-      margin: 0;
-    }
-
-    .container {
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 20px;
+      text-align: center;
     }
 
     .fruit-list {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      grid-gap: 20px;
+      display: flex;
+      justify-content: center;
     }
 
-    .fruit-item {
-      background-color: #f4f4f4;
-      padding: 20px;
-      text-align: center;
+    .fruit-list li {
+      margin: 10px;
+      padding: 10px;
+      border: 1px solid #ccc;
       cursor: pointer;
     }
 
-    .fruit-name {
-      font-size: 20px;
+    .basket {
+      text-align: center;
+      margin-top: 20px;
+    }
+
+    .basket h2 {
       margin-bottom: 10px;
     }
 
-    .fruit-price {
-      font-size: 16px;
-      color: #888;
-    }
-
-    .varieties-list {
-      list-style-type: none;
-      padding: 0;
-      margin-top: 10px;
-      display: none;
-    }
-
-    .variety-item {
-      margin-bottom: 5px;
-    }
-
-    #cart {
+    .payment-method {
       margin-top: 20px;
-      border: 1px solid #ccc;
-      padding: 10px;
+      text-align: center;
     }
 
-    #cart ul {
-      list-style-type: none;
-      padding: 0;
-    }
-
-    #cart li {
-      margin-bottom: 5px;
-    }
-
-    #cart-total {
-      margin-top: 10px;
-      font-weight: bold;
-    }
-
-    #payment-methods {
-      margin-top: 20px;
-    }
-
-    #payment-methods label {
+    .payment-method label {
       margin-right: 10px;
     }
 
-    .item-count {
-      margin-left: 10px;
-      font-size: 12px;
-      color: #888;
+    .total-price {
+      text-align: center;
+      margin-top: 20px;
+    }
+
+    .checkout-button {
+      text-align: center;
+      margin-top: 20px;
     }
   </style>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      // Initialiser les données de vente dans le stockage local
+      var salesData = localStorage.getItem('salesData');
+      if (!salesData) {
+        salesData = {
+          items: {},
+          total: 0,
+          paymentMethod: ''
+        };
+        localStorage.setItem('salesData', JSON.stringify(salesData));
+      } else {
+        salesData = JSON.parse(salesData);
+      }
+
+      // Mettre à jour le panier
+      function updateBasket() {
+        var basket = $('.basket');
+        basket.empty();
+
+        for (var fruit in salesData.items) {
+          var fruitItem = salesData.items[fruit];
+
+          var itemHtml = '<h3>' + fruit + '</h3>';
+
+          for (var variety in fruitItem) {
+            var quantity = fruitItem[variety];
+            itemHtml += '<p>' + variety + ': ' + quantity + '</p>';
+          }
+
+          basket.append(itemHtml);
+        }
+      }
+
+      // Mettre à jour le prix total
+      function updateTotalPrice() {
+        var totalPrice = $('.total-price');
+        totalPrice.text('Prix total: $' + salesData.total.toFixed(2));
+      }
+
+      // Mettre à jour le mode de paiement
+      function updatePaymentMethod(paymentMethod) {
+        salesData.paymentMethod = paymentMethod;
+      }
+
+      // Mettre à jour le stockage local
+      function updateLocalStorage() {
+        localStorage.setItem('salesData', JSON.stringify(salesData));
+      }
+
+      // Gérer le clic sur un fruit
+      $('.fruit-list li').click(function() {
+        var fruit = $(this).data('fruit');
+        var variety = $(this).data('variety');
+
+        if (!salesData.items[fruit]) {
+          salesData.items[fruit] = {};
+        }
+
+        if (!salesData.items[fruit][variety]) {
+          salesData.items[fruit][variety] = 1;
+        } else {
+          salesData.items[fruit][variety]++;
+        }
+
+        salesData.total += 1;
+
+        updateBasket();
+        updateTotalPrice();
+        updateLocalStorage();
+      });
+
+      // Gérer le choix du mode de paiement
+      $('.payment-method input').change(function() {
+        var paymentMethod = $(this).val();
+        updatePaymentMethod(paymentMethod);
+        updateLocalStorage();
+      });
+
+      // Gérer la validation du panier
+      $('.checkout-button button').click(function() {
+        if (salesData.total === 0) {
+          alert('Votre panier est vide. Veuillez ajouter des fruits avant de valider.');
+          return;
+        }
+
+        // Ajouter les ventes dans l'historique
+        var salesHistory = localStorage.getItem('salesHistory');
+        if (!salesHistory) {
+          salesHistory = [];
+        } else {
+          salesHistory = JSON.parse(salesHistory);
+        }
+
+        salesHistory.push(salesData);
+        localStorage.setItem('salesHistory', JSON.stringify(salesHistory));
+
+        // Effacer les données du panier
+        salesData = {
+          items: {},
+          total: 0,
+          paymentMethod: ''
+        };
+        localStorage.setItem('salesData', JSON.stringify(salesData));
+
+        // Rediriger vers la page des détails des ventes
+        window.location.href = 'detailvente.html';
+      });
+
+      // Initialiser l'affichage
+      updateBasket();
+      updateTotalPrice();
+    });
+  </script>
 </head>
 <body>
-  <header>
-    <h1>Vente de Fruits</h1>
-  </header>
-  <div class="container">
-    <div class="fruit-list">
-      <div class="fruit-item" onclick="showVarieties('pomme')">
-        <h2 class="fruit-name">Pomme</h2>
-        <p class="fruit-price">$1.99/kg</p>
-        <ul class="varieties-list" id="pomme-varieties">
-          <li class="variety-item" onclick="addToCart('Pomme', 'Variété 1', 1.99)">Variété 1</li>
-          <li class="variety-item" onclick="addToCart('Pomme', 'Variété 2', 1.99)">Variété 2</li>
-          <li class="variety-item" onclick="addToCart('Pomme', 'Variété 3', 1.99)">Variété 3</li>
-        </ul>
-      </div>
-      <div class="fruit-item" onclick="showVarieties('banane')">
-        <h2 class="fruit-name">Banane</h2>
-        <p class="fruit-price">$0.99/kg</p>
-        <ul class="varieties-list" id="banane-varieties">
-          <li class="variety-item" onclick="addToCart('Banane', 'Variété 1', 0.99)">Variété 1</li>
-          <li class="variety-item" onclick="addToCart('Banane', 'Variété 2', 0.99)">Variété 2</li>
-        </ul>
-      </div>
-      <div class="fruit-item" onclick="showVarieties('orange')">
-        <h2 class="fruit-name">Orange</h2>
-        <p class="fruit-price">$2.49/kg</p>
-        <ul class="varieties-list" id="orange-varieties">
-          <li class="variety-item" onclick="addToCart('Orange', 'Variété 1', 2.49)">Variété 1</li>
-          <li class="variety-item" onclick="addToCart('Orange', 'Variété 2', 2.49)">Variété 2</li>
-          <li class="variety-item" onclick="addToCart('Orange', 'Variété 3', 2.49)">Variété 3</li>
-          <li class="variety-item" onclick="addToCart('Orange', 'Variété 4', 2.49)">Variété 4</li>
-        </ul>
-      </div>
-      <!-- Ajoutez d'autres fruits ici -->
-    </div>
-    <div id="cart">
-      <h2>Panier</h2>
-      <ul id="cart-items"></ul>
-      <div id="cart-total"></div>
-      <div id="payment-methods">
-        <label>
-          <input type="radio" name="payment" value="carte" checked> Carte
-        </label>
-        <label>
-          <input type="radio" name="payment" value="especes"> Espèces
-        </label>
-        <label>
-          <input type="radio" name="payment" value="cheque"> Chèque
-</label>
-</div>
-<button onclick="validateCart()">Valider le panier</button>
-</div>
+  <h1>Vente de Fruits</h1>
+
+  <ul class="fruit-list">
+    <li data-fruit="Banane" data-variety="Cavendish">Banane (Cavendish)</li>
+    <li data-fruit="Banane" data-variety="Plantain">Banane (Plantain)</li>
+    <li data-fruit="Pomme" data-variety="Golden">Pomme (Golden)</li>
+    <li data-fruit="Pomme" data-variety="Granny Smith">Pomme (Granny Smith)</li>
+    <li data-fruit="Orange" data-variety="Valencia">Orange (Valencia)</li>
+    <li data-fruit="Orange" data-variety="Navel">Orange (Navel)</li>
+  </ul>
+
+  <div class="basket">
+    <h2>Panier :</h2>
   </div>
 
-  <script>
-    var cartItems = {}; // Utilisation d'un objet pour stocker les éléments du panier
+  <div class="payment-method">
+    <label for="card">Carte</label>
+    <input type="radio" name="payment" id="card" value="Carte">
 
-    function showVarieties(fruit) {
-      var varietiesList = document.getElementById(fruit + '-varieties');
-      if (varietiesList.style.display === 'none') {
-        varietiesList.style.display = 'block';
-      } else {
-        varietiesList.style.display = 'none';
-      }
-    }
+    <label for="cash">Espèces</label>
+    <input type="radio" name="payment" id="cash" value="Espèces">
 
-    function addToCart(fruit, variety, price) {
-      var cartItemsList = document.getElementById('cart-items');
-      var itemKey = fruit + '-' + variety;
-      
-      if (cartItems[itemKey]) {
-        // Si l'élément existe déjà dans le panier, incrémente le compteur
-        cartItems[itemKey].count++;
-        cartItems[itemKey].element.querySelector('.item-count').textContent = '(' + cartItems[itemKey].count + ')';
-      } else {
-        // Si c'est la première fois que l'élément est ajouté au panier, crée un nouvel élément
-        var newItem = document.createElement('li');
-        newItem.textContent = fruit + ' - ' + variety;
-        var itemCount = document.createElement('span');
-        itemCount.classList.add('item-count');
-        itemCount.textContent = '(1)';
-        newItem.appendChild(itemCount);
-        cartItemsList.appendChild(newItem);
-        
-        cartItems[itemKey] = {
-          count: 1,
-          element: newItem
-        };
-      }
-      
-      updateTotal(price);
-    }
+    <label for="cheque">Chèque</label>
+    <input type="radio" name="payment" id="cheque" value="Chèque">
+  </div>
 
-    function updateTotal(price) {
-      var cartTotal = document.getElementById('cart-total');
-      var total = 0;
-      
-      for (var itemKey in cartItems) {
-        total += price * cartItems[itemKey].count;
-      }
-      
-      cartTotal.textContent = 'Total: $' + total.toFixed(2);
-    }
+  <div class="total-price">
+    Prix total: $0.00
+  </div>
 
-  function validateCart() {
-    // Récupérer les informations du panier
-    var cartItems = document.getElementById('cart-items').children;
-    var paymentMethod = document.querySelector('input[name="payment"]:checked').value;
-    var total = parseFloat(document.getElementById('cart-total').textContent.replace('Total: $', ''));
-
-    // Construire l'objet contenant les informations du panier
-    var salesData = {
-      items: {},
-      paymentMethod: paymentMethod,
-      total: total
-    };
-
-    // Parcourir les éléments du panier et enregistrer les détails des ventes
-    for (var i = 0; i < cartItems.length; i++) {
-      var itemText = cartItems[i].textContent;
-      var itemName = itemText.substring(0, itemText.indexOf('-')).trim();
-      var itemVariety = itemText.substring(itemText.indexOf('-') + 1).trim();
-
-      if (!salesData.items[itemName]) {
-        salesData.items[itemName] = {};
-      }
-
-      if (!salesData.items[itemName][itemVariety]) {
-        salesData.items[itemName][itemVariety] = 1;
-      } else {
-        salesData.items[itemName][itemVariety]++;
-      }
-    }
-
-    // Enregistrer les détails des ventes dans le stockage local (ou toute autre méthode de votre choix)
-    localStorage.setItem('salesData', JSON.stringify(salesData));
-
-    // Naviguer vers la page de détails des ventes
-    window.location.href = 'detailvente.html';
-  }
-
-  </script>
+  <div class="checkout-button">
+    <button>Valider le panier</button>
+  </div>
 </body>
 </html>
