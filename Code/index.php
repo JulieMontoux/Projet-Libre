1297c389cb6b772f1500253061ed1a0a37d1a0d6
Julie montoux <julie.montoux@icloud.com>
2023-06-22 18:35:19 +0200

    vérification qualité du code

--- Code/index.php
+++ Code/index.php
@@ -114,193 +114,191 @@
   </style>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
-  $(document).ready(function() {
-    // Récupérer les fruits, les variétés, les prix et les images depuis la base de données
-    var fruits = [
-      {
-        name: "Pommes",
-        varieties: ["Golden", "Granny", "Reinette", "Royal Gala", "Chantecler", "Braeburn", "Fuji", "Pink Lady"],
-        prices: [3.3, 2.7, 3.6, 2.9, 3.1, 2.9, 3.3, 4.8],
-        images: ["../images/golden.jpg", "../images/granny_smith.jpg", "../images/reinette.jpg", "../images/royal_gala.jpg", "../images/chantecler.jpg", "../images/braeburn.jpg", "../images/fuji.jpg", "../images/pink_lady.jpg"]
-      },
-      {
-        name: "Poires",
-        varieties: ["Williams"],
-        prices: [3.4],
-        images: ["../images/williams.jpg"]
-      },
-      {
-        name: "Cerises",
-        varieties: ["Burlat", "Summit", "Bigalise", "Régina"],
-        prices: [11.65, 8, 15.6, 7.8],
-        images: ["../images/burlat.jpg", "../images/summit.jpg", "../images/bigalise.jpg", "../images/regina.jpg"]
-      },
-      {
-        name: "Kiwis",
-        varieties: ["Hayward"],
-        prices: [2.1],
-        images: ["../images/hayward.jpg"]
-      },
-      {
-        name: "Autres",
-        varieties: ["Confiture", "Jus plat", "Jus pétillant"],
-        prices: [3.90, 2.5, 3],
-        images: ["../images/confiture.jpg", "../images/jus_plat.jpg", "../images/jus_petillant.jpg"]
-      }
-    ];
+    $(document).ready(function() {
+      // Récupérer les fruits, les variétés, les prix et les images depuis la base de données
+      var fruits = [{
+          name: "Pommes",
+          varieties: ["Golden", "Granny", "Reinette", "Royal Gala", "Chantecler", "Braeburn", "Fuji", "Pink Lady"],
+          prices: [3.3, 2.7, 3.6, 2.9, 3.1, 2.9, 3.3, 4.8],
+          images: ["../images/golden.jpg", "../images/granny_smith.jpg", "../images/reinette.jpg", "../images/royal_gala.jpg", "../images/chantecler.jpg", "../images/braeburn.jpg", "../images/fuji.jpg", "../images/pink_lady.jpg"]
+        },
+        {
+          name: "Poires",
+          varieties: ["Williams"],
+          prices: [3.4],
+          images: ["../images/williams.jpg"]
+        },
+        {
+          name: "Cerises",
+          varieties: ["Burlat", "Summit", "Bigalise", "Régina"],
+          prices: [11.65, 8, 15.6, 7.8],
+          images: ["../images/burlat.jpg", "../images/summit.jpg", "../images/bigalise.jpg", "../images/regina.jpg"]
+        },
+        {
+          name: "Kiwis",
+          varieties: ["Hayward"],
+          prices: [2.1],
+          images: ["../images/hayward.jpg"]
+        },
+        {
+          name: "Autres",
+          varieties: ["Confiture", "Jus plat", "Jus pétillant"],
+          prices: [3.90, 2.5, 3],
+          images: ["../images/confiture.jpg", "../images/jus_plat.jpg", "../images/jus_petillant.jpg"]
+        }
+      ];
 
-    var saleDetails = [];
+      var saleDetails = [];
 
-    // Générer la liste de fruits avec les variétés et les images
-    var fruitList = $('.fruit-list');
-    fruits.forEach(function(fruit) {
-      var fruitHtml = '<div class="fruit-card rounded">';
-      fruitHtml += '<img src="' + fruit.images[0] + '" alt="' + fruit.name + '" class="rounded">';
-      fruitHtml += '<h4>' + fruit.name + '</h4>';
-      fruitHtml += '</div>';
+      // Générer la liste de fruits avec les variétés et les images
+      var fruitList = $('.fruit-list');
+      fruits.forEach(function(fruit) {
+        var fruitHtml = '<div class="fruit-card rounded">';
+        fruitHtml += '<img src="' + fruit.images[0] + '" alt="' + fruit.name + '" class="rounded">';
+        fruitHtml += '<h4>' + fruit.name + '</h4>';
+        fruitHtml += '</div>';
 
-      fruitList.append(fruitHtml);
-    });
+        fruitList.append(fruitHtml);
+      });
 
-    // Gérer le clic sur un fruit
-    $('.fruit-card').click(function() {
-      var selectedFruit = $(this).find('h4').text();
-      var selectedVarieties = fruits.find(function(fruit) {
-        return fruit.name === selectedFruit;
-      }).varieties;
-
-      // Générer la liste des variétés pour le fruit sélectionné
-      var varietyList = '<div class="varieties">';
-      selectedVarieties.forEach(function(variety, index) {
-        varietyList += '<div class="variety-card rounded">';
-        varietyList += '<img src="' + fruits.find(function(fruit) {
+      // Gérer le clic sur un fruit
+      $('.fruit-card').click(function() {
+        var selectedFruit = $(this).find('h4').text();
+        var selectedVarieties = fruits.find(function(fruit) {
           return fruit.name === selectedFruit;
-        }).images[index] + '" alt="' + variety + '" class="rounded">';
-        varietyList += '<h5>' + variety + '</h5>';
-        varietyList += '</div>';
-      });
-      varietyList += '</div>';
+        }).varieties;
 
-      // Afficher les variétés pour le fruit sélectionné
-      $(this).siblings('.varieties').remove();
-      $(this).after(varietyList);
-
-      // Gérer le clic sur une variété
-      $('.variety-card').click(function() {
-        var selectedVariety = $(this).find('h5').text();
-        var selectedQuantity = prompt('Quantité vendue :');
-
-        // Vérifier si la quantité saisie est un nombre valide
-        if (selectedQuantity !== null && selectedQuantity !== "" && !isNaN(selectedQuantity)) {
-          selectedQuantity = parseFloat(selectedQuantity);
-          var selectedPrice = fruits.find(function(fruit) {
+        // Générer la liste des variétés pour le fruit sélectionné
+        var varietyList = '<div class="varieties">';
+        selectedVarieties.forEach(function(variety, index) {
+          varietyList += '<div class="variety-card rounded">';
+          varietyList += '<img src="' + fruits.find(function(fruit) {
             return fruit.name === selectedFruit;
-          }).prices[selectedVarieties.indexOf(selectedVariety)];
-          var totalPrice = selectedPrice * selectedQuantity;
+          }).images[index] + '" alt="' + variety + '" class="rounded">';
+          varietyList += '<h5>' + variety + '</h5>';
+          varietyList += '</div>';
+        });
+        varietyList += '</div>';
 
-          addToBasket(selectedFruit, selectedVariety, selectedQuantity, totalPrice);
-          updateTotalPrice();
-        }
-      });
-    });
+        // Afficher les variétés pour le fruit sélectionné
+        $(this).siblings('.varieties').remove();
+        $(this).after(varietyList);
 
-    // Fonction pour ajouter un fruit au panier
-    function addToBasket(fruit, variety, quantity, price) {
-      var basketItem = '<li>' + fruit + ' (' + variety + ') - ' + quantity.toString() + 'kg - ' + price.toFixed(2) + '€</li>';
-      $('.basket ul').append(basketItem);
-      updateTotalPrice(); // Appel de la fonction pour mettre à jour le prix total
-      addToSaleDetails(fruit, variety, quantity, price); // Ajouter les détails de vente
-       // Ajouter les détails de vente au tableau saleDetails
-  addToSaleDetails(fruit, variety, quantity, price);
+        // Gérer le clic sur une variété
+        $('.variety-card').click(function() {
+          var selectedVariety = $(this).find('h5').text();
+          var selectedQuantity = prompt('Quantité vendue :');
 
-// Mettre à jour le panier dans le local storage
-localStorage.setItem('basketItems', JSON.stringify(saleDetails));
-    }
+          // Vérifier si la quantité saisie est un nombre valide
+          if (selectedQuantity !== null && selectedQuantity !== "" && !isNaN(selectedQuantity)) {
+            selectedQuantity = parseFloat(selectedQuantity);
+            var selectedPrice = fruits.find(function(fruit) {
+              return fruit.name === selectedFruit;
+            }).prices[selectedVarieties.indexOf(selectedVariety)];
+            var totalPrice = selectedPrice * selectedQuantity;
 
-    // Fonction pour mettre à jour le prix total
-    function updateTotalPrice() {
-      var total = 0;
-      var basketItems = [];
-
-      $('.basket ul li').each(function() {
-        var quantity = parseFloat($(this).text().split(' - ')[1].split('kg')[0]);
-        var price = parseFloat($(this).text().split(' - ')[2].split('€')[0]);
-
-        if (!isNaN(quantity) && !isNaN(price)) {
-          total += price;
-
-          var item = {
-            fruit: $(this).text().split(' (')[0],
-            variety: $(this).text().split(' (')[1].split(') -')[0],
-            quantity: quantity,
-            price: price.toFixed(2)
-          };
-
-          basketItems.push(item);
-        }
+            addToBasket(selectedFruit, selectedVariety, selectedQuantity, totalPrice);
+            updateTotalPrice();
+          }
+        });
       });
 
-      $('.total-price span').text(total.toFixed(2));
+      // Fonction pour ajouter un fruit au panier
+      function addToBasket(fruit, variety, quantity, price) {
+        var basketItem = '<li>' + fruit + ' (' + variety + ') - ' + quantity.toString() + 'kg - ' + price.toFixed(2) + '€</li>';
+        $('.basket ul').append(basketItem);
+        updateTotalPrice(); // Appel de la fonction pour mettre à jour le prix total
+        addToSaleDetails(fruit, variety, quantity, price); // Ajouter les détails de vente
+        // Ajouter les détails de vente au tableau saleDetails
+        addToSaleDetails(fruit, variety, quantity, price);
 
-      // Sauvegarder les détails du panier dans le stockage local
-      var historique = [];
-      historique.push(localStorage.setItem('basketItems', JSON.stringify(basketItems)));
-    }
+        // Mettre à jour le panier dans le local storage
+        localStorage.setItem('basketItems', JSON.stringify(saleDetails));
+      }
 
-    // Gérer la sélection de la méthode de paiement
-    $('.payment-method button').click(function() {
-      $('.payment-method button').removeClass('active');
-      $(this).addClass('active');
-    });
+      // Fonction pour mettre à jour le prix total
+      function updateTotalPrice() {
+        var total = 0;
+        var basketItems = [];
 
-    // Gérer le clic sur le bouton de validation du panier
-    $('.checkout-button button').click(function() {
-      var paymentMethod = $('.payment-method button.active').text();
-      var totalPrice = parseFloat($('.total-price span').text());
-      if (totalPrice !== 0) {
-        if (paymentMethod === "") {
-          $('.error-message').text('Veuillez choisir une méthode de paiement.');
+        $('.basket ul li').each(function() {
+          var quantity = parseFloat($(this).text().split(' - ')[1].split('kg')[0]);
+          var price = parseFloat($(this).text().split(' - ')[2].split('€')[0]);
+
+          if (!isNaN(quantity) && !isNaN(price)) {
+            total += price;
+
+            var item = {
+              fruit: $(this).text().split(' (')[0],
+              variety: $(this).text().split(' (')[1].split(') -')[0],
+              quantity: quantity,
+              price: price.toFixed(2)
+            };
+
+            basketItems.push(item);
+          }
+        });
+
+        $('.total-price span').text(total.toFixed(2));
+
+        // Sauvegarder les détails du panier dans le stockage local
+        var historique = [];
+        historique.push(localStorage.setItem('basketItems', JSON.stringify(basketItems)));
+      }
+
+      // Gérer la sélection de la méthode de paiement
+      $('.payment-method button').click(function() {
+        $('.payment-method button').removeClass('active');
+        $(this).addClass('active');
+      });
+
+      // Gérer le clic sur le bouton de validation du panier
+      $('.checkout-button button').click(function() {
+        var paymentMethod = $('.payment-method button.active').text();
+        var totalPrice = parseFloat($('.total-price span').text());
+        if (totalPrice !== 0) {
+          if (paymentMethod === "") {
+            $('.error-message').text('Veuillez choisir une méthode de paiement.');
+          } else {
+            // Ajouter le code de redirection vers la page "detailvente" ici
+            window.location.href = 'index.php';
+          }
         } else {
-          // Ajouter le code de redirection vers la page "detailvente" ici
-          window.location.href = 'index.php';
+          $('.error-message').text('Le panier est vide.');
         }
-      } else {
-        $('.error-message').text('Le panier est vide.');
+      });
+
+      // Mettre à jour le tableau des détails de vente
+      function updateSaleDetailsTable() {
+        var tableBody = $('#detail-vente-body');
+        tableBody.empty();
+
+        saleDetails.forEach(function(detail) {
+          var newRow = '<tr>';
+          newRow += '<td>' + detail.fruit + '</td>';
+          newRow += '<td>' + detail.variety + '</td>';
+          newRow += '<td>' + detail.quantity + '</td>';
+          newRow += '<td>' + detail.price + '€</td>';
+          newRow += '</tr>';
+
+          tableBody.prepend(newRow);
+        });
+      }
+
+      // Ajouter les détails de vente à la liste
+      function addToSaleDetails(fruit, variety, quantity, price) {
+        var detail = {
+          fruit: fruit,
+          variety: variety,
+          quantity: quantity,
+          price: price.toFixed(2)
+        };
+
+        saleDetails.push(detail);
+        updateSaleDetailsTable();
       }
     });
-
-    // Mettre à jour le tableau des détails de vente
-    function updateSaleDetailsTable() {
-      var tableBody = $('#detail-vente-body');
-      tableBody.empty();
-
-      saleDetails.forEach(function(detail) {
-        var newRow = '<tr>';
-        newRow += '<td>' + detail.fruit + '</td>';
-        newRow += '<td>' + detail.variety + '</td>';
-        newRow += '<td>' + detail.quantity + '</td>';
-        newRow += '<td>' + detail.price + '€</td>';
-        newRow += '</tr>';
-
-        tableBody.prepend(newRow);
-      });
-    }
-
-    // Ajouter les détails de vente à la liste
-    function addToSaleDetails(fruit, variety, quantity, price) {
-      var detail = {
-        fruit: fruit,
-        variety: variety,
-        quantity: quantity,
-        price: price.toFixed(2)
-      };
-
-      saleDetails.push(detail);
-      updateSaleDetailsTable();
-    }
-  });
-</script>
-
+  </script>
 </head>
 
 <body>
